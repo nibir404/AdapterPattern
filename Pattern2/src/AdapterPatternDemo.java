bd5b72f9b1296a5be959388aa5bab0c23951c9ef
Nibir Imtiaz <nibirimtiaz1@gmail.com>
2019-11-16 21:48:58 +0600

    Add files via upload

---
+++ Pattern2/src/AdapterPatternDemo.java
@@ -0,0 +1,7 @@
+public abstract class AdapterPatternDemo {
+    public static void main(String[] args) {
+        CreditCard targetInterface = new BankCustomer();
+        targetInterface.giveBankDetails();
+        System.out.println(targetInterface.getCreditCard());
+    }
+}
---
+++ Pattern2/src/CreditCard.java
@@ -0,0 +1,4 @@
+public interface CreditCard {
+    void giveBankDetails();
+    String getCreditCard();
+}
